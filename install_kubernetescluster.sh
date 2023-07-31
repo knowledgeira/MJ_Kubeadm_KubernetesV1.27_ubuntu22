250a0e2acbbd982a2fa2a8ede8ba9304e4629aea
Manoj <67874453+knowledgeira@users.noreply.github.com>
2023-07-31 17:26:26 +0530

    Update install_kubernetescluster.sh

--- install_kubernetescluster.sh
+++ install_kubernetescluster.sh
@@ -3,9 +3,9 @@
 BLUE='\033[0;34m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
-
+clear
 if lscpu | grep -E 'CPU\(s\)|Thread|Core' &>/dev/null; then
-    echo -e "STEP 1 : ${BLUE}CPU, Thread, and Core information:${NC}"
+    echo -e "\n\nSTEP 1 : ${BLUE}CPU, Thread, and Core information:${NC}"
     echo "------------------------------------------------"
 
     lscpu | grep -E 'CPU\(s\)|Thread|Core'
@@ -16,41 +16,41 @@ else
 fi
 
 if lscpu | grep -i -E 'svm|vmx' &>/dev/null; then
-    echo -e "STEP 2 : \n\n${GREEN}Virtualization Status : It is enabled.${NC}"
+    echo -e "\n\n${GREEN}STEP 2:Virtualization Status : It is enabled.${NC}"
     echo "------------------------------------------------"
 
 else
-    echo -e "STEP 2 : ${GREEN}Virtualization Status: Not enabled.${NC}"
+    echo -e "\n\n${GREEN}STEP 2 :Virtualization Status: Not enabled.${NC}"
     echo "------------------------------------------------"
 
 fi
 
 if ! command -v vagrant &> /dev/null; then
-    echo -e "\n\n ${BLUE}Vagrant is not installed. Installing...${NC}"
+    echo -e "\n\n ${BLUE}STEP 3 :Vagrant is not installed. Installing...${NC}"
     echo "------------------------------------------------"
 
     sudo apt update
     sudo apt install vagrant -y
 else
-    echo -e "\n\n${GREEN}You already have Vagrant installed. Moving forward.${NC}"
+    echo -e "STEP \n\n${GREEN} STEP 3 :You already have Vagrant installed. Moving forward.${NC}"
     echo "------------------------------------------------"
 
 fi
 
 # Check if VirtualBox is installed
 if ! command -v virtualbox &> /dev/null; then
-    echo -e "\n\n${BLUE}VirtualBox is not installed. Installing...${NC}"
+    echo -e "\n\n${BLUE}STEP 4 :VirtualBox is not installed. Installing...${NC}"
     echo "------------------------------------------------"
 
     sudo apt update
     sudo apt install virtualbox -y
 else
-    echo -e "\n\n${GREEN}You already have VirtualBox installed. Moving forward...${NC}"
+    echo -e "\n\n${GREEN}STEP 4 :You already have VirtualBox installed. Moving forward...${NC}"
     echo "------------------------------------------------"
 
 fi
 
-echo -e "Step 4: \n\n${BLUE}Deploying Kubernetes cluster${NC}"
+echo -e "Step 5: \n\n${BLUE}Deploying Kubernetes cluster${NC}"
 echo "------------------------------------------------"
-
+sleep 30
 vagrant up
