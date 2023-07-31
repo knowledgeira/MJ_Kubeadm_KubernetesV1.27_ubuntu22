c70c023f0d51c2e08937179ba908822857bea196
Manoj <67874453+knowledgeira@users.noreply.github.com>
2023-07-31 16:57:45 +0530

    Update install_kubernetescluster.sh
    Added Visualization Check

--- install_kubernetescluster.sh
+++ install_kubernetescluster.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/bash
+
+
+if lscpu | grep -E 'CPU\(s\)|Thread|Core' &>/dev/null; then
+    echo -e "CPU, Thread, and Core information:\n\n"
+    lscpu | grep -E 'CPU\(s\)|Thread|Core'
+else
+    echo "Unable to get CPU, Thread, and Core information."
+fi
+
+if lscpu | grep -i -E 'svm|vmx' &>/dev/null; then
+    echo -e "\n\nVirtualization is enabled.\n\n"
+else
+    echo -e "Virtualization is not enabled.\n\n"
+fi
+
+
+
+
 if ! command -v vagrant &> /dev/null; then
     echo -e "\033[0;31mVagrant is not installed. Installing...\033[0m"
     sudo apt update
