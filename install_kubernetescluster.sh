cac3ae0a29b79d1fdca2aa5d74cc2aa344432e78
Manoj <67874453+knowledgeira@users.noreply.github.com>
2023-07-31 17:10:53 +0530

    Update install_kubernetescluster.sh

--- install_kubernetescluster.sh
+++ install_kubernetescluster.sh
@@ -1,41 +1,39 @@
 #!/usr/bin/bash
 
+BLUE='\033[0;34m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 
 if lscpu | grep -E 'CPU\(s\)|Thread|Core' &>/dev/null; then
-    echo -e "CPU, Thread, and Core information:\n\n"
+    echo -e "${BLUE}CPU, Thread, and Core information:${NC}\n\n"
     lscpu | grep -E 'CPU\(s\)|Thread|Core'
 else
-    echo "Unable to get CPU, Thread, and Core information."
+    echo -e "${BLUE}Unable to get CPU, Thread, and Core information.${NC}"
 fi
 
 if lscpu | grep -i -E 'svm|vmx' &>/dev/null; then
-    echo -e "\n\nVirtualization is enabled.\n\n"
+    echo -e "\n\n${GREEN}Virtualization is enabled.${NC}\n\n"
 else
-    echo -e "Virtualization is not enabled.\n\n"
+    echo -e "${GREEN}Virtualization is not enabled.${NC}\n\n"
 fi
 
-
-
-
 if ! command -v vagrant &> /dev/null; then
-    echo -e "\033[0;31mVagrant is not installed. Installing...\033[0m"
+    echo -e "${BLUE}Vagrant is not installed. Installing...${NC}"
     sudo apt update
     sudo apt install vagrant -y
 else
-    GREEN='\033[0;32m'
-    NC='\033[0m' # No Color
     echo -e "${GREEN}You already have Vagrant installed. Moving forward.${NC}"
 fi
 
 # Check if VirtualBox is installed
 if ! command -v virtualbox &> /dev/null; then
-    echo -e "\033[0;31mVirtualBox is not installed. Installing...\033[0m"
+    echo -e "${BLUE}VirtualBox is not installed. Installing...${NC}"
     sudo apt update
     sudo apt install virtualbox -y
 else
     echo -e "${GREEN}You already have VirtualBox installed. Moving forward...${NC}"
 fi
 
-echo -e "Making Virtual Machines with Kubernetes Components"
-vagrant up
+echo -e "${BLUE}Making Virtual Machines with Kubernetes Components${NC}"
 
+vagrant up
